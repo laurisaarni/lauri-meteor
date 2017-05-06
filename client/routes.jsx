1d888da51fdb0ff687a5008de94180fe0789d39e
Lauri Matti Saarni <lauri@web-essentials.asia>
2017-05-07 00:35:17 +0700

    [TASK] todo sample

--- client/routes.jsx
+++ client/routes.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {mount} from 'react-mounter'
 import {MainLayout} from './layouts/MainLayout'
 import App from './App'
+import '../lib/startup/accounts-config.js';
+
 
 FlowRouter.route('/', {
   action () {
--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import '../lib/api/tasks.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
